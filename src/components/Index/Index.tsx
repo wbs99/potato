5aa50663f5dd49ff9b662626d2b3883a0bd89f58
dingtalk_xsgrpl <1134954328@qq.com>
2022-04-04 21:58:28 +0800

    useEffect getTodoList

--- src/components/Index/Index.tsx
+++ src/components/Index/Index.tsx
@@ -35,16 +35,6 @@ const Index = () => {
     navigate('/login')
   }
 
-  // function handleButtonClick(e) {
-  //   message.info('Click on left button.')
-  //   console.log('click left button', e)
-  // }
-
-  // function handleMenuClick(e) {
-  //   message.info('Click on menu item.')
-  //   console.log('click', e)
-  // }
-
   const menu = (
     <Menu>
       <Menu.Item key="1" icon={<UserOutlined />}>
--- src/components/Todos/Todos.tsx
+++ src/components/Todos/Todos.tsx
@@ -1,11 +1,36 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import TodoInput from './TodoInput'
 import todoApi from '../../api/Todos/Todos'
 import { message } from 'antd'
 import './Todos.scss'
 
+type todoListType = {
+  completed: boolean
+  completed_at: null
+  created_at: string
+  deleted: boolean
+  description: string
+  extra: object
+  id: number
+  updated_at: string
+  user_id: number
+}
+
 const ToDos: React.FC = () => {
   const [description, setDescription] = useState('')
+  const [todoList, setTodoList] = useState<todoListType[]>([])
+
+  useEffect(() => {
+    getTodoList()
+  }, [])
+
+  const getTodoList = () => {
+    const getTodoList = async () => {
+      const res = await todoApi.getTodoList()
+      setTodoList(res.data.resources)
+    }
+    getTodoList()
+  }
 
   const addTodo = (value: string) => {
     value === ''
@@ -13,6 +38,7 @@ const ToDos: React.FC = () => {
       : todoApi.addTodo({ description: value }).then(() => {
           setDescription('')
           message.success('成功添加新任务')
+          getTodoList()
         })
   }
 
@@ -25,6 +51,15 @@ const ToDos: React.FC = () => {
           addTodo(value)
         }}
       />
+      <main>
+        {todoList.map(item => {
+          return (
+            <div key={item.id}>
+              <p>{item.description}</p>
+            </div>
+          )
+        })}
+      </main>
     </div>
   )
 }
